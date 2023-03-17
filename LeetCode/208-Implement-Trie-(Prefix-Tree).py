5a5405d498c01b3fa967e0443e203054d3263043
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2023-03-17 12:56:48 -0700

    Leetcode #208: Trie

---
+++ LeetCode/208-Implement-Trie-(Prefix-Tree).py
@@ -0,0 +1,7 @@
+from LeetCode.CustomStructures import Trie
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
--- LeetCode/CustomStructures.py
+++ LeetCode/CustomStructures.py
@@ -38,3 +38,49 @@ class DSU:
     
     def connected(self, x, y):
         return self.find(x) == self.find(y)
+
+class Trie:    
+    class TrieNode:
+        def __init__(self):
+            self.children = {}
+            self.end = False
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Trie.TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Trie.TrieNode()
+            node = node.children[c]
+        node.end = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
