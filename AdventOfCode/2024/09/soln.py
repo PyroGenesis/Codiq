cfec87b5cddd356d4f2785f97c2e0456d9370ba3
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2024-12-10 12:00:32 -0800

    Advent Of Code 2024: Day 9 (p2)

--- AdventOfCode/2024/09/soln.py
+++ AdventOfCode/2024/09/soln.py
@@ -2,11 +2,12 @@ import os
 
 # paths
 here = os.path.dirname(os.path.abspath(__file__))
-filepath = os.path.join(here, 'input.txt')
+filepath = os.path.join(here, "input.txt")
 
 # read input
-with open(filepath, mode='r', encoding='utf8') as f:
+with open(filepath, mode="r", encoding="utf8") as f:
     data = f.read()
+
 
 def part1():
     disk = [int(x) for x in data.strip()]
@@ -16,7 +17,7 @@ def part1():
 
     i = 0
     # j should always be on a valid file block
-    j = n-1 if n%2==1 else n-2
+    j = n - 1 if n % 2 == 1 else n - 2
     while j > 0 and disk[j] == 0:
         j -= 2
 
@@ -28,7 +29,7 @@ def part1():
                 checksum += block * id
                 block += 1
             i += 1
-        
+
         elif disk[i] > 0:
             # actual space block
             id = j // 2
@@ -38,12 +39,13 @@ def part1():
             disk[j] -= 1
             if disk[j] == 0:
                 j -= 2
-        
+
         else:
             # empty space block
             i += 1
 
     print(checksum)
+
 
 class FileNode:
     id: int
@@ -56,71 +58,147 @@ class FileNode:
         self.size = size
         self._prev_node = None
         self._next_node = None
-    
+
+    @property
+    def is_file_node(self):
+        return self.id >= 0
+
+    @property
+    def is_space_node(self):
+        return self.id < 0
+
     @property
     def next_node(self):
         return self._next_node
-    
+
     @next_node.setter
     def next_node(self, other: "FileNode | None"):
+        # if a <=> c, and connect a to b, self is a
+        assert other is not None, "next_node(): other is None"
+        # b -> c
+        other._next_node = self._next_node
+        if other._next_node is not None:
+            # b <- c
+            other._next_node._prev_node = other
+        # a <- b
+        other._prev_node = self
+        # a -> b
         self._next_node = other
-        if other is not None:
-            other._prev_node = self
-    
+
     @property
     def prev_node(self):
         return self._prev_node
-    
+
     @prev_node.setter
     def prev_node(self, other: "FileNode | None"):
+        # if a <=> c, and connect b to c, self is c
+        assert other is not None, "prev_node(): other is None"
+        # a <- b
+        other._prev_node = self._prev_node
+        if other._prev_node is not None:
+            # a -> b
+            other._prev_node._next_node = other
+        # b -> c
+        other._next_node = self
+        # b <- c
         self._prev_node = other
-        if other is not None:
-            other._next_node = self
+
+    def detach(self):
+        if self._prev_node:
+            self._prev_node._next_node = self._next_node
+        if self.next_node:
+            self._next_node._prev_node = self._prev_node
+        self._prev_node = None
+        self._next_node = None
+
+    def __repr__(self):
+        if self.id < 0:
+            this = f"S({self.size})"
+        else:
+            this = f"F({self.id},{self.size})"
+        if self._next_node:
+            this += " -> " + repr(self._next_node)
+        return this
 
 
 def part2():
-    head = FileNode(-1, 0)
-    node = head
+    _dummy = FileNode(-1, 0)
+    node = _dummy
     for i, ch in enumerate(data.strip()):
         size = int(ch)
-        if size == 0:
-            continue
-
         if i % 2 == 0:
             # file
             node.next_node = FileNode(i // 2, size)
         else:
             # space
-            node.next_node = FileNode(-i, size)
+            node.next_node = FileNode(-(i + 1), size)
         node = node.next_node
+    head = _dummy.next_node
+    _dummy.detach()
     tail = node
 
-    # first space is immediately after first node
-    space_node = head.next_node
+    # # first space is immediately after first node
+    # space_node = head.next_node
+    # while space_node:
+    #     while space_node and (space_node.id >= 0 or space_node.size < 1):
+    #         space_node = space_node.next_node
+    #     if not space_node:
+    #         break
 
-    min_file_size = 1
-    while space_node:
-        if space_node.size < min_file_size:            
-            while space_node and space_node.id >= 0:
-                if space_node.id == file_node.id:
-                    space_node = None
-                else:
-                    space_node = space_node.prev_node
-            continue
+    #     file_node = tail
+    #     # try to find a file node that is smaller than space node
+    #     while file_node and (file_node.id < 0 or file_node.size > space_node.size):
+    #         if file_node.id == space_node.id:
+    #             file_node = None
+    #         else:
+    #             file_node = file_node.prev_node
+    #     if not file_node:
+    #         break
 
-        file_node = tail
-        while file_node:
-            while file_node and file_node.id < 0:
-                if file_node.id == space_node.id:
-                    file_node = None
-                else:
-                    file_node = file_node.prev_node
-            if not file_node:
-                break
+    #     if tail == file_node:
+    #         tail = file_node.prev_node
+    #     file_node.detach()
+    #     space_node.prev_node = file_node
+    #     space_node.size -= file_node.size
 
-            if file_node.size <= space_node.size:
+    file_node = tail if tail.is_file_node else tail.prev_node
+    while file_node and file_node.prev_node:
+        _temp_prev = file_node.prev_node
+
+        space_node = head.next_node
+        while space_node and (space_node.is_file_node or space_node.size < file_node.size):
+            if space_node.id == file_node.id:
+                # there is no satisfyable space
+                space_node = None
+            else:
+                space_node = space_node.next_node
+
+        if space_node:
+            if _temp_prev:
+                _temp_prev.size += file_node.size
+            file_node.detach()
+            space_node.prev_node = file_node
+            space_node.size -= file_node.size
+
+        file_node = _temp_prev
+        while file_node and file_node.is_space_node:
+            file_node = file_node.prev_node
+
+    checksum = 0
+    block = 0
+    node = head
+    while node:
+        if node.id > 0:
+            # Sn    = n/2 [2a + (n-1)d]
+            #   Since d == 1
+            #       = n/2 [2a + (n-1)]
+            #       = na + n(n-1)/2
+            checksum += node.id * (node.size * block + (node.size * (node.size - 1) // 2))
+        block += node.size
+        node = node.next_node
+
+    print(checksum)
 
 
-
-
-    
+# part1()
+part2()
